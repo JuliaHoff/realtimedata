92c5047efb8b55d630cce0ce25583121854787b3
JuliaHoff <Julia@Julias-MacBook-Pro.local>
2017-03-06 13:12:02 +0100

    all code
    all code

---
+++ js.js
@@ -0,0 +1,24 @@
+
+var counter =0;
+window.setInterval(
+function () {
+counter = counter + 1;
+document.getElementById("counter").innerHTML = counter;
+}, 0,001);
+
+// 26042 per sek
+
+var counter2 =0;
+window.setInterval(
+function () {
+counter2 = counter2 + 1;
+document.getElementById("counter2").innerHTML = counter2;
+}, 250);
+
+
+var counter3=0;
+window.setInterval(
+function () {
+counter3 = counter3 +0;
+document.getElementById("counter3").innerHTML = counter3;
+}, 0);
